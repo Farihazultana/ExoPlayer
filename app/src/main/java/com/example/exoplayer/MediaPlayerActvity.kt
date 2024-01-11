558601d08984908c542756dca03537728fc6d445
farihazultana <farihasultana237@gmail.com>
2024-01-11 16:36:22 +0600

    seekbar

--- app/src/main/java/com/example/exoplayer/MediaPlayerActvity.kt
+++ app/src/main/java/com/example/exoplayer/MediaPlayerActvity.kt
@@ -3,7 +3,10 @@ package com.example.exoplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import com.example.exoplayer.databinding.ActivityMediaPlayerActvityBinding
 import com.example.exoplayer.databinding.CustomExoLayoutBinding
@@ -12,7 +15,7 @@ class MediaPlayerActvity : AppCompatActivity() {
     private lateinit var binding: ActivityMediaPlayerActvityBinding
     private lateinit var binding2: CustomExoLayoutBinding
     private lateinit var player: ExoPlayer
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @OptIn(UnstableApi::class) override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMediaPlayerActvityBinding.inflate(layoutInflater)
         binding2 = CustomExoLayoutBinding.inflate(layoutInflater)
@@ -47,10 +50,37 @@ class MediaPlayerActvity : AppCompatActivity() {
         player.addMediaItem(secondItem)
         player.addMediaItem(thirdItem)
 
+
+
         // Prepare the player.
         player.prepare()
         // Start the playback.
         //player.play()
+
+        // SeekBar
+        player.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(state: Int) {
+                if (state == Player.STATE_READY) {
+                    val duration = player.duration
+                    val scaledValue = duration / 1000f
+                    binding2.seekBar.value = 0f
+                    binding2.seekBar.valueFrom = 0f
+                    binding2.seekBar.valueTo = scaledValue
+
+
+                    binding2.seekBarStart.text = "0.00f"
+                    binding2.seekbarEnd.text = scaledValue.toString()
+
+                }
+            }
+
+            override fun onPositionDiscontinuity(reason: Int) {
+                val currentPosition = player.currentPosition
+                binding2.seekBar.value = currentPosition / 1000f
+            }
+
+        })
+
 
         binding2.ivPlay.setOnClickListener {
             player.play()
