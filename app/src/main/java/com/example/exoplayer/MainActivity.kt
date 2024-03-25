60cc9353a5569e0e65184906edeb30e0a4970bb8
farihazultana <farihasultana237@gmail.com>
2024-03-25 02:04:29 -0400

    bug solved

--- app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -69,6 +69,9 @@ class MainActivity : AppCompatActivity(){
 
     override fun onStart() {
         super.onStart()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(broadcastReceiver, it)
+        }
         bindService(Intent(this, MusicPlayerService::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
         startMusicPlayerService()
     }
